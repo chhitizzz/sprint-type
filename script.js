de71b83973dadabb39f77acec5c42b80ce3ef030
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-24 13:49:43 +0545

    wip

---
+++ script.js
@@ -0,0 +1,6 @@
+const words = 'ten forgive consider smoke descriptive vanish faulty bee wakeful induce send look marriage amount unbecoming'.split(' ');
+
+function newGame() {
+    document.getElementById(elementId: 'words').innerHTML = '';
+    
+}
