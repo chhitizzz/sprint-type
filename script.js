0e625cc5057d4c25ef5abcdaf89aa1977329855b
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-24 13:57:57 +0545

    js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const words = 'ten forgive consider smoke descriptive vanish faulty bee wakeful induce send look marriage amount unbecoming'.split(' ');
+const wordsCount = words.length;
+
+function randomWord() {
+    const randomIndex =  Math.ceil(Math.random() * wordsCount);
+    return words[randomIndex];
+}
 
 function newGame() {
-    document.getElementById(elementId: 'words').innerHTML = '';
-    
+    document.getElementById('words').innerHTML = '';
+    for(let i = 0; i < 200; i++){
+        document.getElementById('words').innerHTML += randomWord;
+    }
 }
+
+newGame();
