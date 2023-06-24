6f8881dfbfc079d931b30c3725a5f5c60c004d28
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-24 14:20:06 +0545

    gg

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ function randomWord() {
 }
 
 function formatWord(word) {
-    return `<div class='word'>${word}</div>`
+    return `<div class='word'>
+            <span class='letter'>
+            ${word.split('').join('</span><span class="letter">')}
+            </span>
+            </div>`;
 }
 
 function newGame() {
