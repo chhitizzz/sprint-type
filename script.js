6cbd02ea314d5fd5f28745c84b7ba684727d1328
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-26 21:28:38 +0545

    js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const words = 'ten eleven neeva nivia chhitiz subhu bijaya merina sabina manchester united mamba marriage amount unbecoming plot room knife instruct haircut badge doubt position omen meddle mountain stone interest overwatch talented brake cake hair jump announce deadlock deafening temporary somber cobweb stem thick trucks remind zippy bedroom doubtful sock disagree cabbage brother gentle political tense tub mundane light drag lock deeply history language stupid lumpy toothsome imagine sable roll lunch damp decide bushes desire knife thaw knowledgeable skirt heartbreaking agreement roasted shy harass pinch bizarre load hospitable awesome passenger steel wonder middle shave accurate want nippy'.split(' ');
 const wordsCount = words.length;
 
+function addClass(el, name){
+    el.className += ' '+name;
+}
+
+function removeClass(el, name){
+    el.className = el.className.replace(name, '');
+}
+
 function randomWord() {
     const randomIndex =  Math.ceil(Math.random() * wordsCount);
     return words[randomIndex];
@@ -19,10 +27,11 @@ function newGame() {
     for(let i = 0; i < 200; i++){
         document.getElementById('words').innerHTML += formatWord(randomWord());
     }
+    document.querySelector('.word');
 }
 
 document.getElementById('game').addEventListener('keyup', ev => {
-    console.log(ev);
+    const key = ev.key;
 })
 
 newGame();
