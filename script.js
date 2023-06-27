08a0cb155e18c22d6d8fa0f90c1219e36409ccbe
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-27 22:01:17 +0545

    js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.getElementById('game').addEventListener('keyup', ev => {
     const key = ev.key;
     const currentWord = document.querySelector('.word.current');
     const currentLetter = document.querySelector('.letter.current');
-    const expected = currentLetter.innerHTML;
+    const expected = currentLetter?.innerHTML || ' ';
     const isLetter = key.length === 1 && key !== ' ';
     const isSpace = key === ' ';
 
